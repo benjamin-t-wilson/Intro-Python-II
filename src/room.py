5c6edc72ebc5ffb129f838e2a2bfd61a3cf7a4c2
benjamin-t-wilson <benjiimanwillson@gmail.com>
2020-05-14 19:56:12 -0700

    added items class and related functions

--- src/room.py
+++ src/room.py
@@ -3,9 +3,13 @@
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=[]):
         self.name = name
         self.description = description
+        self.items = items
+
+    def items_list(self):
+        return [item.__str__() for item in self.items]
 
     def __str__(self):
         return f"You have entered room: {self.name}. It is {self.description}"
